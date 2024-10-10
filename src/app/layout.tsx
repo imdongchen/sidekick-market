d51dcd48283b3f4a9f3868bd174c129ad22f2270
Dong Chen <imdongchen@gmail.com>
2024-10-10 16:16:29 -0700

    add privacy policy pdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: {
     template: '%s - Sidekick',
-    default: 'Sidekick - a swim app for teams',
+    default: 'Sidekick - A swim app for teams',
   },
 }
 
